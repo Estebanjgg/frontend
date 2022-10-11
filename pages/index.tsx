88d7bb9ffecbc12f2f8b783ca54c314471955a25
Esteban <esteban030991@gmail.com>
2022-10-11 20:35:02 -0300

    integraçao com banco de dados

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,68 @@
-import { Box } from '@mui/material'
-import type { NextPage } from 'next'
-import { Professor } from '../src/@types/professor'
-import Lista from '../src/components/Lista/Lista'
-
-
-
-
-
+import { Box, Dialog, DialogActions, Grid, TextField, Button, Snackbar } from "@mui/material";
+import type { NextPage } from "next";
+import { Professor } from "../src/@types/professor";
+import Lista from "../src/components/Lista/Lista";
+import { useIndex } from "../src/hooks/pages/useIndex";
 
 const Home: NextPage = () => {
-  const professores: Professor[] =[
-    {
-      id: 1,
-      nome: "Esteban Gonzalez",
-      foto:"https://github.com/Estebanjgg.png",
-      descricao:"Descricação do profressor 1",
-      valor_hora: 100
-
-    },
-    {
-      id: 2,
-      nome: "Professor 2",
-      foto:"https://github.com/Estebanjgg.png",
-      descricao:"Descricação do profressor 2",
-      valor_hora: 200
-
-    },
-    {
-      id: 3,
-      nome: "Professor 3",
-      foto:"https://github.com/solangelis.png",
-      descricao:"Descricação do profressor 3",
-      valor_hora: 300
-
-    },
-    {
-      id: 4,
-      nome: "Professor 4",
-      foto:"https://github.com/Solangelis.png",
-      descricao:"Descricação do profressor 4",
-      valor_hora: 400
-
-    }
-  ]
+  const { listaProfessores, 
+    nome, 
+    setNome, 
+    email, 
+    setEmail, 
+    professorSelecionado,
+    setProfessorSelecionado,
+    marcarAula,
+    mensagem,
+    setMensagem
+  } = useIndex();
 
   return (
-    <Box sx={{ backgroundColor: '#f4f4f4' }}>
-      <Lista professores={professores}></Lista>
-    </Box>
-  )
-}
+    <div>
+      <Box sx={{ backgroundColor: "#f4f4f4" }}>
+        <Lista 
+        professores={listaProfessores}
+        onSelect={(professor) => setProfessorSelecionado(professor)}
+        ></Lista>
+      </Box>
 
-export default Home
+      <Dialog onClose={() => setProfessorSelecionado(null)} open={professorSelecionado !== null } fullWidth PaperProps={{ sx: { p: 5 } }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField 
+            label="Digito o nome" 
+            type="text" 
+            fullWidth 
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+            />
+            
+          </Grid>
+          <Grid item xs={12}>
+            <TextField 
+            label="Digito o email" 
+            type="email" 
+            fullWidth 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            />
+          </Grid>
+        </Grid>
+
+        <DialogActions sx={{mt: 5}}>
+          <Button onClick={() => setProfessorSelecionado(null)}>Cancelar</Button>
+          <Button onClick={() => marcarAula()}>Marcar</Button>
+        </DialogActions>
+      </Dialog>
+    <Snackbar 
+    message={mensagem} 
+    open={mensagem.length > 0}  
+    autoHideDuration={2500}
+    onClose={()=> setMensagem('')} 
+    />
+
+    </div>
+  );
+};
+
+export default Home;
--- src/components/Lista/Lista.tsx
+++ src/components/Lista/Lista.tsx
@@ -1,31 +1,44 @@
 import { Button } from "@mui/material";
 import { Professor } from "../../@types/professor";
-import { Descricao, Foto, Informacoes, ItemLista, ListaStyled, ListaVazia, Nome, Valor } from "./Lista.style";
+import { FormatadorService } from "../../services/FormatadorService";
+import {
+  Descricao,
+  Foto,
+  Informacoes,
+  ItemLista,
+  ListaStyled,
+  ListaVazia,
+  Nome,
+  Valor,
+} from "./Lista.style";
 
 interface ListaProps {
-  professores: Professor[],
+  professores: Professor[];
+  onSelect: (Professor : Professor) => void
 }
 
 const Lista = (props: ListaProps) => {
   return (
     <div>
-        {props.professores.length > 0 ? (
-              <ListaStyled>
-              {props.professores.map(professor => (
-                <ItemLista key={professor.id}>
-                  <Foto src={professor.foto}></Foto>
-                  <Informacoes>
-                    <Nome>{professor.nome}</Nome>
-                    <Valor>{professor.valor_hora.toLocaleString('pt-BR', { minimumFractionDigits: 2, style:'currency', currency: 'BRL' })} por hora</Valor>
-                    <Descricao>{professor.descricao}</Descricao>
-                    <Button sx={{ width: "70%" }}>Marcar Aula com Esteban</Button>
-                  </Informacoes>
-                </ItemLista>
-              ))}
-            </ListaStyled>
-        ) : (
-            <ListaVazia>Nenhum item encontrado</ListaVazia>   
-        )}
+      {props.professores.length > 0 ? (
+        <ListaStyled>
+          {props.professores.map(professor => (
+            <ItemLista key={professor.id}>
+              <Foto src={professor.foto}></Foto>
+              <Informacoes>
+                <Nome>{professor.nome}</Nome>
+                <Valor>
+                  {FormatadorService.valorMonetario(professor.valor_hora)}por hora
+                </Valor>
+                <Descricao>{FormatadorService.limitarTexto(professor.descricao, 200)}</Descricao>
+                <Button onClick={() => props.onSelect(professor)} sx={{ width: "70%" }}>Marcar Aula com {professor.nome}</Button>
+              </Informacoes>
+            </ItemLista>
+          ))}
+        </ListaStyled>
+      ) : (
+        <ListaVazia>Nenhum item encontrado</ListaVazia>
+      )}
     </div>
   );
 };
